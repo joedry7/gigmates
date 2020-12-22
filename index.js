707054c553ac0de5d6b5dfe13d387553f42706c0
Joseph Dry <joe.dry6477@gmail.com>
2020-12-22 13:47:13 -0500

    Removed a comment that was not needed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const app = require('./server/app');
 const config = require('./config');
-// const knex = require('./server/knex');
 
 app.listen(config.PORT, () => console.log(`Server is listening on port ${config.PORT}`));
