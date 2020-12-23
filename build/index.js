11b6298d47ce2c9ccea9a732df1f1c785ebfb952
Joseph Dry <joe.dry6477@gmail.com>
2020-12-23 10:07:25 -0500

    Cleaned up ignored files

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("./server/app"));
-const config_1 = __importDefault(require("./config"));
-app_1.default.listen(config_1.default.PORT, () => console.log(`Server is listening on port ${config_1.default.PORT}`));
