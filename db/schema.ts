2c01623fc171f80b957ed665a6b80ee8a7c643e8
Joseph Dry <joe.dry6477@gmail.com>
2020-12-24 12:19:28 -0500

    Refactored schema file to use async await

--- db/schema.ts
+++ db/schema.ts
@@ -1,38 +1,50 @@
 import { default as knex } from '../server/knex';
 
-const createUsersTable = () => knex.raw('DROP TABLE IF EXISTS users CASCADE')
-  .then(() => knex.schema.createTable('users', (table) => {
-    table.increments('id')
-      .primary();
-    table.string('username', 25);
-    table.string('first_name', 25);
-    table.string('last_name', 25);
-  }))
-  .catch((err) => {
-    console.log(`Error creating users table: ${err}`);
-  });
+const createUsersTable = async () => {
+  try {
+    await knex.raw('DROP TABLE IF EXISTS users CASCADE')
+    await knex.schema.createTable('users', (table) => {
+      table.increments('id')
+        .primary();
+      table.string('username', 25);
+      table.string('first_name', 25);
+      table.string('last_name', 25);
+    });
+  } catch(err) {
+    console.log(`Error creating Users tables: ${err}`);
+  }
+};
 
-const createPostsTable = () => knex.schema.dropTableIfExists('posts')
-  .then(() => knex.schema.createTable('posts', (table) => {
-    table.increments('id')
-      .primary();
-    table.integer('user_id')
-      .unsigned();
-    table.foreign('user_id')
-      .references('users.id')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE');
-    table.text('content');
-    table.timestamps(false, true);
-  }))
-  .catch((err) => {
+const createPostsTable = async () => {
+  try {
+    await knex.schema.dropTableIfExists('posts')
+    await knex.schema.createTable('posts', (table) => {
+      table.increments('id')
+        .primary();
+      table.integer('user_id')
+        .unsigned();
+      table.foreign('user_id')
+        .references('users.id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      table.text('content');
+      table.timestamps(false, true);
+    });
+  } catch(err) {
     console.log(`Error creating posts table: ${err}`);
-  });
+  }
+};
 
-const buildSchema = () => new Promise<void>((res) => res())
-  .then(createUsersTable)
-  .then(createPostsTable)
-  .catch((err) => console.log(`Error building schema: ${err}`))
-  .then(() => knex.destroy(() => console.log('Schema finished')));
+const buildSchema = async () => {
+  try {
+  await createUsersTable();
+  await createPostsTable();
+  } catch(err) {
+    console.log(`Error building schema: ${err}`)
+  } finally {
+    await knex.destroy();
+    console.log(`Schema finished`);
+  }
+};
 
 buildSchema();
