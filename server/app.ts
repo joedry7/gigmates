6731e23180d05982a3e8646cd003c084c1fa0597
Joseph Dry <joe.dry6477@gmail.com>
2020-12-24 14:57:24 -0500

    React is being compiled and rendered

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import router from './routes';
+import path from 'path';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/../public')));
+app.use(morgan('tiny'));
 
 app.use('/api/user', router.user);
 
